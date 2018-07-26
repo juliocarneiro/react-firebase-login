9cad7f15e5bebdb82da31f8182b842599da80ed4
Júlio Carneiro <juliocarneiro@gmail.com>
2018-07-26 15:11:45 -0300

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCFQFTnwzPJ0i7br2C64UVIoCaMVLzKZEk",
-  authDomain: "juliocarneir.firebaseapp.com",
-  databaseURL: "https://juliocarneir.firebaseio.com",
-  projectId: "juliocarneir",
-  storageBucket: "juliocarneir.appspot.com",
-  messagingSenderId: "558152189358"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 }
 
 const fire = firebase.initializeApp(config)
